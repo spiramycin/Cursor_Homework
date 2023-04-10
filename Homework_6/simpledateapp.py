5d662be72d12ee10df6a76679cf70adab6fa41a7
spiramycin <127058767+spiramycin@users.noreply.github.com>
2023-04-10 15:15:17 +0300

    Add files via upload

---
+++ Homework_6/simpledateapp.py
@@ -0,0 +1,30 @@
+from flask import Flask, abort
+from datetime import datetime, timedelta, timezone
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def basic():
+    app.logger.info('Reached hello route')
+    return '<h1>Good start</h1>'
+
+@app.route('/datetime/')
+@app.route('/datetime/<string:tz>')
+def get_time(tz=None):
+    if tz:
+        try:
+            tz = timezone(timedelta(hours=int(tz)))
+        except ValueError:
+            abort(406, 'Please choose correct timezone')
+    current_time = datetime.now(tz=tz).strftime('Current date %d-%m-%Y and time %H:%M:%S  %z')
+    return current_time
+
+
+
+if __name__ == '__main__':
+    import logging
+    app.logger.setLevel(logging.DEBUG)
+
+
+    app.run(host='127.0.0.1', port=5000, debug=True)
